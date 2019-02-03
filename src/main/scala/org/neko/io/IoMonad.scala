226cfe52a00dd1ea5966b9863c73ab55956142dd
Heku Watada <heku.watada@gmail.com>
2019-02-03 19:23:10 +0000

    Added stdout-stdin program with IO Monad.

--- src/main/scala/org/neko/io/IoMonad.scala
+++ src/main/scala/org/neko/io/IoMonad.scala
@@ -2,10 +2,13 @@ package org.neko.io
 
 import cats.effect.IO
 
+//@see https://typelevel.org/blog/2017/05/02/io-monad-for-cats.html
 object IoMonad extends App {
 
   val program: IO[Unit] = for {
-    _ <- IO { println("hello") }
+    _ <- IO { println("please type") }
+    input <- IO { scala.io.StdIn.readLine }
+    _ <- IO { println(s"received: $input")}
   } yield ()
 
   program.unsafeRunSync()
